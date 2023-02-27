bc74ebefc4639c387580728a64caa6c35ccc32fe
BasakK6 <katukbasak@gmail.com>
2023-02-27 22:59:55 +0300

    feature: created a network change manager with connectivity_plus and riverpod packages

---
+++ lib/connectivity_change/network_result.dart
@@ -0,0 +1,20 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
+
+enum NetworkResult{
+  on,
+  off,
+  unknown;
+
+  static NetworkResult checkConnectivity(ConnectivityResult result){
+    switch(result){
+      case ConnectivityResult.wifi:
+      case ConnectivityResult.ethernet:
+      case ConnectivityResult.mobile:
+        return NetworkResult.on;
+      case ConnectivityResult.bluetooth:
+      case ConnectivityResult.none:
+      default:
+        return NetworkResult.off;
+    }
+  }
+}
